2b03f1e50f7bb75d0817bf59f1f910f36a6a1f2c
Gabrielius Volkovas <gabrielius@scoding.com>
2020-11-19 16:53:48 +0200

    Initial commit

---
+++ app/Models/User.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    /** @var string[] */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /** @var string[] */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /** @var array */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
